df1b0e469ab6b3071d7adadfec634723429573f7
steventhijm1-gif <steventhijm1@gmail.com>
2026-08-02 00:30:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// ============================
+// JARVIS OS v1.0
+// Core Script
+// ============================
 
+console.log("JARVIS OS gestart.");
+
+const micButton = document.getElementById("micButton");
+const developerButton = document.getElementById("developerMode");
+
+micButton.addEventListener("click", () => {
+    alert("🎤 Voice System komt in versie 2.");
+});
+
+developerButton.addEventListener("click", () => {
+    alert("⚙️ Developer Mode komt binnenkort.");
+});
